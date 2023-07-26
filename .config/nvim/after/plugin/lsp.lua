6446674432f2d03386fb56466df92887961bcdc4
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2023-07-27 00:48:11 +0200

    keybind for reformat

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -3,7 +3,7 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-	'clangd'
+  'clangd'
 })
 
 -- Fix Undefined global 'vim'
@@ -11,12 +11,12 @@ lsp.nvim_workspace()
 
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<CR>'] = cmp.mapping.confirm({select = true}),
-    ["<C-Space>"] = cmp.mapping.complete(),
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
 })
 
 cmp_mappings['<S-Tab>'] = nil
@@ -26,18 +26,18 @@ lsp.setup_nvim_cmp({
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+  suggest_lsp_servers = false,
+  sign_icons = {
+    error = 'E',
+    warn = 'W',
+    hint = 'H',
+    info = 'I'
+  }
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-  local telescopeOpts = {noremap = true, silent = true}
+  local opts = { buffer = bufnr, remap = false }
+  local telescopeOpts = { noremap = true, silent = true }
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -54,12 +54,11 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "gd", function() require "telescope.builtin".lsp_definitions() end, opts)
   vim.keymap.set("n", "gi", function() require "telescope.builtin".lsp_implementations() end, opts)
   vim.keymap.set("n", "<leader>o", function() vim.cmd.ClangdSwitchSourceHeader() end, opts)
+  vim.keymap.set("n", "<leader>cf", function() vim.lsp.buf.format({ async = true }) end, opts);
 end)
 
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true
+  virtual_text = true
 })
-
-
