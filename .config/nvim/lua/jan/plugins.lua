ec34a4d9e707ac8a34c0ee5b8a6ae5dfa0c2a6e4
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2024-05-30 19:12:25 +0200

    Added telescope to nvim

--- .config/nvim/lua/jan/plugins.lua
+++ .config/nvim/lua/jan/plugins.lua
@@ -10,4 +10,10 @@ return require('packer').startup(function(use)
     }
   end
 }
+use {
+  'nvim-telescope/telescope.nvim', tag = '0.1.6',
+-- or                            , branch = '0.1.x',
+  requires = { {'nvim-lua/plenary.nvim'} }
+}
+
 end)
--- .config/nvim/lua/jan/remap.lua
+++ .config/nvim/lua/jan/remap.lua
@@ -2,3 +2,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>`", "<C-^>")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
